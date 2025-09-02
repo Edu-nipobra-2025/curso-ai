a95a587c922ec48d1f7852c68f0b70ebe6fb594e
Riudy Eduardo Uto <riudyeduardo@gmail.com>
2025-09-02 16:15:02 -0300

    tarefinha bacana git push origin main git add. git add .  git f f ff a a

---
+++ tarefa 6.py
@@ -0,0 +1,10 @@
+n1 = int(input("Digite um número:"))
+n2 = 12
+
+if n1 > n2:
+    print("O n1 é maior que n2")
+elif n1 == n2:
+    print ("Os dois números são iguais")
+else:
+    print("O N2 é maior que N1")
+    
