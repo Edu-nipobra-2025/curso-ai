9a0d2a795c324f48ec02e01f0a4ee50611d9453e
Riudy Eduardo Uto <riudyeduardo@gmail.com>
2025-09-08 17:12:13 -0300

    mudanças

--- aula2/import bibliotecamate.py
+++
@@ -1,4 +0,0 @@
-import bibliotecamate
-
-a = 1
-b = 3
---
+++ aula3(Silvio)/tarefa1.py
@@ -0,0 +1,53 @@
+import math as m 
+
+def ler_real(msg: str) -> float:
+    while True: 
+     try:
+        return float(input(msg).replace(",","."))
+     except ValueError:
+        print("Entrada Inválida")
+
+    
+def soma (a: float, b: float) -> float:
+           return a+b
+def subtra (a: float, b: float) -> float:
+           return a-b
+def multi (a: float, b: float) -> float:
+           return a*b
+def div (a: float, b: float) -> float:
+           return a/b
+     
+while True:
+ print("\n ----Menu----")
+ print("\n 1) Adição")
+ print("\n 2) Subtração")
+ print("\n 3) Multiplicação")
+ print("\n 4) Divisão")
+ print("\n 0) Fim")
+ opcao = int(input("\n Escolha a conta que você irá fazer."))
+
+
+
+ if opcao == 1:
+    a = int(input("Dê primeiro Valor\n"))
+    b = int(input("Dê segundo Valor\n"))
+    print(soma(a,b))
+ elif opcao == 2:
+    a = int(input("Dê primeiro Valor\n"))
+    b = int(input("Dê segundo Valor\n"))
+    print(subtra(a,b))
+ elif opcao == 3:
+    a = int(input("Dê primeiro Valor\n"))
+    b = int(input("Dê segundo Valor\n"))
+    print(multi(a,b))
+ elif opcao == 4:
+  if b == 0:
+   print("Valor inválido!")
+  else:
+      print(div(a,b))
+
+ elif opcao ==0:
+     print("Fim do programa.")
+     break
+ else:
+      print("Opção inválida!")
