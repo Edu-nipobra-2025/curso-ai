83a7fd6d9c2da6dbe7fc264e3ed88c22e20bad3d
Riudy Eduardo Uto <riudyeduardo@gmail.com>
2025-09-12 14:31:45 -0300

    dontyoufightmebaby

--- aula5(Silvio)/tarefa5.py
+++ aula5(Silvio)/tarefa5.py
@@ -1,22 +1,27 @@
-maria = 0
-joao = 0
-candidato = [maria, joao]
+candidatos = ["maria", "joao"]
+votagem = []
 
-votoJoao = maria
-votoMaria = joao
-votoTotal = 0
 while True:
- votagem = input("vote por algum canditado (maria ou joao), caso queira acabar a votação, escreva fim ")
- if votagem == "joao":
-  votoJoao += 1
-  votoTotal += 1
-  continue 
- elif votagem == "maria":
-  votoMaria += 1
-  votoTotal += 1
- if votagem == "fim":
-  "Acabou a votação!"
-  break
+    votagemResultado = input("Vote por algum candidato (maria ou joao). Caso queira acabar a votação, escreva 'fim': ")
+    
+    if votagemResultado == "fim":
+        print("Acabou a votação!")
+        break
+    elif votagemResultado in candidatos:
+        votagem.append(votagemResultado)
+    else:
+        print("Candidato inválido, apenas escreva 'maria' ou 'joao'.")
 
-print("Votos joão: ",votoJoao,"\nVotos Maria: ",votoMaria,"\nVotos Totais: ",votoTotal)
-  
+print("\nTodos os votos:", ", ".join(votagem))
+votosMaria = votagem.count("maria")
+votosJoao = votagem.count("joao")
+print("Votos Maria:", votosMaria)
+print("Votos João:", votosJoao)
+print("Votos Totais:", len(votagem))
+
+if votosMaria > votosJoao:
+    print("Maria venceu a eleição!")
+elif votosJoao > votosMaria:
+    print("João venceu a eleição!")
+else:
+    print("Empate!")
